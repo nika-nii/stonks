13a1ebf2d047d0211880cd92e4ce64afd2e9792b
Fateev-student <50704141+Fateev-student@users.noreply.github.com>
2021-03-01 00:17:29 +0300

    работы с бд так и нет

--- filter/app/database/models.py
+++ filter/app/database/models.py
@@ -5,10 +5,10 @@ from .database import Base
 
 
 class Observers(Base):
-    __tablename__ = "users"
+    __tablename__ = "observers"
 
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, unique=True, index=True)
     event = Column(String)
-    watch = Column(int)
+    watch = Column(Integer)
     currency = Column(String)
--- filter/app/main.py
+++ filter/app/main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, requests, Depends
 from pydantic import BaseModel
-from database.database import engine
+from database.database import engine, SessionLocal
 from database import models
+from sqlalchemy.orm import Session
+from database.models import Observers
 import datetime
+
 
 class Message(BaseModel):
 	time: datetime.datetime
@@ -17,22 +20,36 @@ class Observer(BaseModel):
     watch: int
     currency: str
 
+
 app = FastAPI()
 
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 models.Base.metadata.create_all(bind=engine)
+
 
 @app.post("/add")
 async def send_value(message: Message):
     return message
 
+
 @app.post("/watch")
 async def add_observer(observer: Observer):
     return observer
 
+
 @app.get("/watch")
-async def get_observers_list():
+async def get_observers_list(db: Session = Depends(get_db()), skip: int = 0, limit: int = 100):
     #должна быть подключена БД, должны возвращаться данные из нее
-    return 'work in progress'
+    return db.query(models.Item).offset(skip).limit(limit).all()
+
 
 @app.get("/watch/{id}")
 async def observer():
