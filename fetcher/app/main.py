dc7f1fb3df6c447ce3ef68a9b8988a74275c9a0b
Farakshin Nikita <farakshin.nikita@gmail.com>
2021-03-07 17:30:12 +0300

    Fetcher updates

--- fetcher/app/main.py
+++ fetcher/app/main.py
@@ -6,10 +6,15 @@ import schedule
 
 import requests
 
+from dotenv import load_dotenv
+# Загружаем параметры
+load_dotenv()
+
+period = int(os.getenv('PERIOD_SECS'))
 
 def get_data_from_api():
     print("I'm working...")
-    r = requests.get('https://api.exchangeratesapi.io/latest')
+    r = requests.get(FOREIGN_API)
     data = r.json()
     rates = data['rates']
     date = datetime.strptime(rates['date'], "%Y-%m-%d")
@@ -19,8 +24,9 @@ def get_data_from_api():
             "currency": rate,
             "time": date
         }
-        requests.post(os.getenv('FOREIGN_API'), data=json.dumps(message, default=str))
-        #заменить запрос в env
+        requests.post(
+            os.getenv(STORAGE_API),
+            data=json.dumps(message, default=str)
+        )
 
-
-schedule.every().day.do(get_data_from_api)
+schedule.every(period).seconds.do(get_data_from_api)
