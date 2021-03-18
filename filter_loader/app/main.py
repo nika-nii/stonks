5322ad849849fa2f874cc181d5d0ed51051d9fdd
nika-nii <farakshin.nikita@gmail.com>
2021-03-18 18:51:20 +0300

    kabanchik -> filter_loader

--- filter_loader/app/main.py
+++ filter_loader/app/main.py
@@ -11,7 +11,7 @@ load_dotenv()
 host1 = "http://storage/"
 host2 = "http://filter/add"
 
-def podskok():
+def job():
     r = requests.get(f'{host1}currencies')
     for cur in r.json():
         r = requests.get(f'{host1}latest/{cur}')
@@ -22,7 +22,7 @@ def podskok():
             data=json.dumps(data, default=str)
         )
 
-schedule.every(60).seconds.do(podskok)
+schedule.every(60).seconds.do(job)
 
 while True:
     schedule.run_pending()
